119b79d56a7a08b018766da2080cdd4ddc6718c7
joao.simao <vitoralves1604@gmail.com>
2024-07-05 01:13:07 -0300

    initial commit

---
+++ PhoneBook.Application/DTOS/PhoneBookDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Application.DTOS
+{
+    public class PhoneBookDTO
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Phone { get; set; }
+    }
+}
